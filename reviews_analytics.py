cb4d5dd136d9839316106e6bfe5acee1c008de0e
Yuchang Fan <yuchangjimmyfan@gmail.com>
2021-08-09 22:44:38 +0800

    add 'good' filter

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -15,13 +15,19 @@ print('資料讀取完了，總共有', len(data), '筆留言。')
 sum_len = 0
 for review in data:
 	sum_len += len(review) # sum_len = sum_len + len(review)
-
 print('每則留言平均有', sum_len / len(data), '個字')
 
+# 篩選小於100個字的留言
 new = []
 for review in data:
 	if len(review) < 100:
 		new.append(review)
-
 print('一共有', len(new), '筆留言長度小於100個字')
-print(new[0])
+
+# 篩選留言裡有提到good
+
+good = []
+for review in data:
+	if 'good' in review:
+		good.append(review)
+print('一共有', len(good), '筆留言提到good')
