a0603340681a639e990a4aaf9bcb459e8d248e64
Yuchang Fan <yuchangjimmyfan@gmail.com>
2021-08-09 22:31:21 +0800

    calc

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -8,6 +8,12 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('資料讀取完了，總共有', len(data), '筆留言。')
 
-print(data[0])
+# 計算留言平均長度
+
+sum_len = 0
+for review in data:
+	sum_len += len(review) # sum_len = sum_len + len(review)
+
+print('每則留言平均有', sum_len / len(data), '個字')
